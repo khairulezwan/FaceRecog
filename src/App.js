a8f1bb5e5e9f9b2199855cf73da022c73ea444b0
Khairul Ezwan <khairulezwan2015@gmail.com>
2020-02-09 22:52:09 +0800

    backbone for Logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLink from './components/ImageLink/ImageLink';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Navigation/>
-        {/* <Logo />
+        <Logo/>
         <ImageLink />
-        <Facerecognition /> */}
+        {/* <Facerecognition /> */}
     </div>
   );
 }
