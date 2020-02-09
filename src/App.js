f9685394d86121796e59ed26dc434afde5776568
Khairul Ezwan <khairulezwan2015@gmail.com>
2020-02-09 23:35:08 +0800

    adding imagelink and rank

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLink from './components/ImageLink/ImageLink';
+
 import './App.css';
+import Particles from 'react-particles-js';
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value:200,
+      density:{
+        enable:true,
+        value_area:800,
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+    <Particles className='particles'
+              params={particlesOptions}
+            />
         <Navigation/>
         <Logo/>
+        <Rank/>
         <ImageLink />
+
         {/* <Facerecognition /> */}
     </div>
   );
