eabd4ebc268f5f86233f1f6109d420dfb7093b92
Khairul Ezwan <khairulezwan2015@gmail.com>
2020-02-10 01:37:40 +0800

    1

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,7 @@ const FaceRecognition = ({imageUrl}) => {
     return(
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img alt='results' src={imageUrl} width='500px' height='auto'/>
+                <img id='inputImage' alt='results' src={imageUrl} width='500px' height='auto'/>
             </div>
         </div>
     )
