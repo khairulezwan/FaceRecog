abfddcf24d4915a7dd19304f40e3cf99b653bdc2
Khairul Ezwan <khairulezwan2015@gmail.com>
2020-02-11 00:26:49 +0800

    Updates

---
+++ precache-manifest.36b9280c032e27564aab35729e83fe2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c95ce19aa9b1de51c4c63f5bba46be2",
+    "url": "/FaceRecog/index.html"
+  },
+  {
+    "revision": "abeb395b68986094e67d",
+    "url": "/FaceRecog/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d705e3488dd2d695792c",
+    "url": "/FaceRecog/static/css/main.0e54fe86.chunk.css"
+  },
+  {
+    "revision": "abeb395b68986094e67d",
+    "url": "/FaceRecog/static/js/2.2e9efd58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FaceRecog/static/js/2.2e9efd58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d705e3488dd2d695792c",
+    "url": "/FaceRecog/static/js/main.e8798577.chunk.js"
+  },
+  {
+    "revision": "6edbe8dcb74f6e1beb39",
+    "url": "/FaceRecog/static/js/runtime-main.969ce57c.js"
+  }
+]);
